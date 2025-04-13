b9d157c4af5babd519f241ebba23ac7a991cc8e5
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-04-13 21:17:16 +0500

    CHANGE: Trading pairs screenshots removed

--- src/pages/Home/TradingPairs/RightToLeft.jsx
+++ src/pages/Home/TradingPairs/RightToLeft.jsx
@@ -20,62 +20,13 @@ import OP from "../../../assets/images/home/trading_pairs/OP.webp";
 import KDA from "../../../assets/images/home/trading_pairs/KDA-1.webp";
 import AutoCarousel from "../../../components/AutoCarousel";
 import CarouselRenderItem from "../../../components/CarouselRenderItem";
-import AAC from "../../../assets/images/home/trading_pairs/america-and-canada.jpg";
-import Screenshot2 from "../../../assets/images/home/trading_pairs/Screenshot2.png";
-import Screenshot3 from "../../../assets/images/home/trading_pairs/Screenshot3.png";
-import Screenshot4 from "../../../assets/images/home/trading_pairs/Screenshot4.png";
-import Screenshot5 from "../../../assets/images/home/trading_pairs/Screenshot5.png";
-import Screenshot6 from "../../../assets/images/home/trading_pairs/Screenshot6.png";
-import Screenshot8 from "../../../assets/images/home/trading_pairs/Screenshot8.png";
-import Screenshot9 from "../../../assets/images/home/trading_pairs/Screenshot9.png";
-import Screenshot10 from "../../../assets/images/home/trading_pairs/Screenshot10.png";
-import Screenshot11 from "../../../assets/images/home/trading_pairs/Screenshot11.png";
-import Screenshot12 from "../../../assets/images/home/trading_pairs/Screenshot12.png";
-import Screenshot13 from "../../../assets/images/home/trading_pairs/Screenshot13.png";
-import Screenshot14 from "../../../assets/images/home/trading_pairs/Screenshot14.png";
-import Screenshot15 from "../../../assets/images/home/trading_pairs/Screenshot15.png";
-import Screenshot16 from "../../../assets/images/home/trading_pairs/Screenshot16.png";
-import Screenshot17 from "../../../assets/images/home/trading_pairs/Screenshot17.png";
-import Screenshot18 from "../../../assets/images/home/trading_pairs/Screenshot18.png";
-import Screenshot19 from "../../../assets/images/home/trading_pairs/Screenshot19.png";
-import Screenshot20 from "../../../assets/images/home/trading_pairs/Screenshot20.png";
-import Screenshot21 from "../../../assets/images/home/trading_pairs/Screenshot21.png";
-import Screenshot22 from "../../../assets/images/home/trading_pairs/Screenshot22.png";
-import Screenshot23 from "../../../assets/images/home/trading_pairs/Screenshot23.png";
-import Screenshot24 from "../../../assets/images/home/trading_pairs/Screenshot24.png";
 
 const shuffleArray = (array) => {
   return [...array].sort(() => Math.random() - 0.5);
 };
 
 const RightToLeft = () => {
-  const screenshots = [
-    Screenshot2,
-    Screenshot3,
-    Screenshot4,
-    Screenshot5,
-    Screenshot6,
-    Screenshot8,
-    Screenshot9,
-    Screenshot10,
-    Screenshot11,
-    Screenshot12,
-    Screenshot13,
-    Screenshot14,
-    Screenshot15,
-    Screenshot16,
-    Screenshot17,
-    Screenshot18,
-    Screenshot19,
-    Screenshot20,
-    Screenshot21,
-    Screenshot22,
-    Screenshot23,
-    Screenshot24,
-  ];
-
   const otherImages = [
-    AAC,
     ETH,
     PEPE,
     SOL,
@@ -96,7 +47,7 @@ const RightToLeft = () => {
     KDA,
   ];
 
-  const tradingPairImages = shuffleArray([...screenshots, ...otherImages]);
+  const tradingPairImages = shuffleArray([...otherImages]);
 
   return (
     <div>
