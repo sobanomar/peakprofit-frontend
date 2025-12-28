b1ece28775e04309a8b523d47096e5eaa606f7e7
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-12-29 00:44:35 +0500

    commented out testimonial video

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       <Evaluation />
       <WhyChoosePPF />
       <RawSpreads />
-      <TraderTestimonials />
+      {/* <TraderTestimonials /> */}
       <StillNotConvinced />
     </>
   );
