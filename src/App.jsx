b51721344db3f8b5c758c37c5b6b1009a3451398
Soban Umar <sobanumar0@gmail.com>
2026-03-11 02:17:49 +0500

    feat: Introduce Lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Lenis from "@studio-freight/lenis";
 import "./App.css";
 import Router from "./Router";
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Router />
